e7c88717f41c4c967f38b6c3c85a5b55d9e31d97
sy8000 <fsbydz@vip.qq.com>
2020-04-24 17:47:09 +0800

    Initial commit

---
+++ src/main/java/cn/besbing/Conctroller/IndexController.java
@@ -0,0 +1,18 @@
+package cn.besbing.Conctroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/main")
+    public String FowardIndex(){
+        return "maintain";
+    }
+
+    @RequestMapping("/content")
+    public String content(){
+        return "logintest";
+    }
+}
