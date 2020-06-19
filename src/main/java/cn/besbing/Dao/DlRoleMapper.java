f6b37246399524a0bb489bfae05c8b21f1cd2019
sy8000 <fsbydz@vip.qq.com>
2020-06-19 14:48:58 +0800

    解决了pom中enpwd的version，加入了dl_role和dl_permission表的mapper

---
+++ src/main/java/cn/besbing/Dao/DlRoleMapper.java
@@ -0,0 +1,30 @@
+package cn.besbing.Dao;
+
+import cn.besbing.Entities.DlRole;
+import cn.besbing.Entities.DlRoleExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface DlRoleMapper {
+    long countByExample(DlRoleExample example);
+
+    int deleteByExample(DlRoleExample example);
+
+    int deleteByPrimaryKey(String roleid);
+
+    int insert(DlRole record);
+
+    int insertSelective(DlRole record);
+
+    List<DlRole> selectByExample(DlRoleExample example);
+
+    DlRole selectByPrimaryKey(String roleid);
+
+    int updateByExampleSelective(@Param("record") DlRole record, @Param("example") DlRoleExample example);
+
+    int updateByExample(@Param("record") DlRole record, @Param("example") DlRoleExample example);
+
+    int updateByPrimaryKeySelective(DlRole record);
+
+    int updateByPrimaryKey(DlRole record);
+}
